a0ce45c7110911360ad1553eabc09c2ceafea1ae
rsugubland343 <blandr@usf.edu>
2026-08-26 10:42:51 -0400

    add main.py via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+############################################
+# Remington Bland # blandr@usf.edu         #
+#------------------------------------------#
+# main.py: prints "Hello, World!" and      #
+# examines behavior of data types          #
+############################################
+
+"""we want this script to PRINT the string Hello, World!
+"""
+print("Hello World!")
+
+a = "hello world!"
+print(a)
+
+i = 13
+j = 15
+k = (i + j)
+print(k)
+
+x = 4.3
+y = 3.6
+z = (x * y)
+print(k - z)
